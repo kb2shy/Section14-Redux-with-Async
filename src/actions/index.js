492eef9639fde5f40eae90af6f53d9ec1036946b
kb2shy <chhaianpin@hotmail.com>
2019-07-10 13:01:47 -0700

    the second way of fetching posts and users without lodash memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,17 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// Good memoization code so fetches aren't made each time for a post article
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
 
 // learning memoization - not the correct way
 // export const fetchUser = function(id) {
@@ -19,3 +25,11 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
 //     dispatch({ type: 'FETCH_USER', payload: response.data });
 //   });
 // };
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+  // console.log(userIds);
+  // await dispatch(fetchUser());
+}

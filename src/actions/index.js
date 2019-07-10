6b1738f3dbfd2523306cf79cd28c2239a03637eb
kb2shy <chhaianpin@hotmail.com>
2019-07-10 13:07:06 -0700

    refactored fetchPostsAndUsers to implement lodash chain function

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,8 +28,14 @@ export const fetchUser = (id) => async dispatch => {
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+  .map('userId')
+  .uniq()
+  .forEach(id => dispatch(fetchUser(id)))
+  .value()
   // console.log(userIds);
   // await dispatch(fetchUser());
 }
